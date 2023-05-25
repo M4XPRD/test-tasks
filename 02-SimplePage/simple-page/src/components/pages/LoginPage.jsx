0954dedfe4c551527d4e5bcaa9e2d3ac050985c3
Max <ppmax@bk.ru>
2023-05-25 23:22:28 +0300

    Formik added

--- 02-SimplePage/simple-page/src/components/pages/LoginPage.jsx
+++ 02-SimplePage/simple-page/src/components/pages/LoginPage.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
 import * as yup from 'yup';
 import cn from 'classnames';
 import routes from '../../routes/routes';
 
 const LoginPage = () => {
-  const [inputValues, setInputValues] = useState({ login: '', password: '' });
-  const [inputErrors, setInputErrors] = useState({ login: false, password: false });
   const [showHelp, setShowHelp] = useState(false);
   const loginRef = useRef();
   const passwordFocus = useRef();
@@ -33,6 +32,18 @@ const LoginPage = () => {
       .required(),
   });
 
+  const f = useFormik({
+    initialValues: {
+      login: '',
+      password: '',
+    },
+    validationSchema: loginSchema,
+    validateOnChange: true,
+    onSubmit: () => {
+      navigate(routes.profile());
+    },
+  });
+
   const handleKeyDown = (event, focus) => {
     if (event.key === 'ArrowDown' || event.key === 'Tab') {
       event.preventDefault();
@@ -55,58 +66,29 @@ const LoginPage = () => {
     }
   };
 
-  const handleInput = (e) => {
-    const { name, value } = e.target;
-    setInputValues({ ...inputValues, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    try {
-      loginSchema.validateSync(inputValues, { abortEarly: false });
-      setInputErrors({ login: false, password: false });
-      navigate(routes.profile());
-    } catch (error) {
-      switch (error.message) {
-        case 'LoginError':
-          setInputValues({ ...inputValues, login: '' });
-          setInputErrors({ login: true, password: false });
-          break;
-        case 'PasswordError':
-          setInputValues({ ...inputValues, password: '' });
-          setInputErrors({ login: false, password: true });
-          break;
-        default:
-          setInputValues({ ...inputValues, login: '', password: '' });
-          setInputErrors({ login: true, password: true });
-          break;
-      }
-    }
-  };
-
   const loginInput = cn('form-input', 'input-focus', {
-    'is-invalid': inputErrors.login,
+    'is-invalid': f.errors.login,
   });
 
   const passwordInput = cn('form-input', 'input-focus', {
-    'is-invalid': inputErrors.password,
+    'is-invalid': f.errors.password,
   });
 
   return (
     <div className="form-container">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={f.handleSubmit}>
         <div>
           <input
             className={loginInput}
             ref={loginRef}
             onKeyDown={(e) => handleKeyDown(e, loginRef)}
-            onChange={handleInput}
+            onChange={f.handleChange}
             type="text"
             name="login"
-            placeholder={inputErrors.login ? 'Incorrect Login' : 'Login'}
+            placeholder={f.errors.login ? 'Incorrect Login' : 'Login'}
             autoComplete="off"
             spellCheck="false"
-            value={inputValues.login}
+            value={f.values.login}
           />
         </div>
         <div>
@@ -114,18 +96,19 @@ const LoginPage = () => {
             className={passwordInput}
             ref={passwordFocus}
             onKeyDown={(e) => handleKeyDown(e, passwordFocus)}
-            onChange={handleInput}
+            onChange={f.handleChange}
             type="password"
             name="password"
-            placeholder={inputErrors.password ? 'Incorrect Password' : 'Password'}
+            placeholder={f.errors.password ? 'Incorrect Password' : 'Password'}
             autoComplete="off"
             spellCheck="false"
-            value={inputValues.password}
+            value={f.values.password}
           />
         </div>
         <div className="form-submit">
           <input
             className="form-input"
+            disabled={f.errors.login || f.errors.password}
             ref={submitFocus}
             onKeyDown={(e) => handleKeyDown(e, submitFocus)}
             type="submit"
@@ -140,7 +123,7 @@ const LoginPage = () => {
         onClick={() => setShowHelp(!showHelp)}
         onKeyDown={(e) => handleKeyDown(e, helpFocus)}
       >
-        {showHelp ? 'developer21 : 123456' : 'Need some help?'}
+        {showHelp ? 'developer21 : 123456' : 'Get your login here!'}
       </button>
     </div>
   );
