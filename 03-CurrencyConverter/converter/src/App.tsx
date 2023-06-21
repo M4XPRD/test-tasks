66198165cd1bfdc000e2a8fa46bc5eecc2fa78f3
Max <ppmax@bk.ru>
2023-06-21 03:58:59 +0300

    Core created

--- 03-CurrencyConverter/converter/src/App.tsx
+++ 03-CurrencyConverter/converter/src/App.tsx
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import routes from './routes/index';
+import video from './assets/3.mp4';
+import MainPage from './components/pages/MainPage';
+import CurrenciesPage from './components/pages/CurrenciesPage';
+import ErrorPage from './components/pages/ErrorPage';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>src/App.tsx </code>
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+  <div className="main">
+    <div className="overlay" />
+    <div className="container">
+      <video autoPlay loop muted controlsList="nodownload" className="background-clip">
+        <source src={video} type="video/mp4" />
+      </video>
+      <Routes>
+        <Route path={routes.main()} element={<MainPage />} />
+        <Route path={routes.currencies()} element={<CurrenciesPage />} />
+        <Route path={routes.error()} element={<ErrorPage />} />
+      </Routes>
+    </div>
   </div>
 );
 
