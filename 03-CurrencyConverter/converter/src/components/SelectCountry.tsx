0d71b39e0d985a853981b21b857f8967e4c09d9e
Max <ppmax@bk.ru>
2023-06-23 02:59:25 +0300

    Types added, logic fixed

--- 03-CurrencyConverter/converter/src/components/SelectCountry.tsx
+++ 03-CurrencyConverter/converter/src/components/SelectCountry.tsx
@@ -1,33 +1,69 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import {
-  Autocomplete, Box, Grid, TextField,
+  Autocomplete, Grid, TextField,
 } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../slices';
+import { SetExchangeType } from '../slices/currenciesSlice';
 
-const SelectCountry = (props: { currencyValue: string; label: string; setCurrency: any }) => {
-  const { currencyValue, label, setCurrency } = props;
+interface Props {
+  currencyValue: string;
+  label: string;
+  setExchange: SetExchangeType;
+}
+
+interface SetExchangeAction {
+  type: string;
+  payload: {
+    newValue: string;
+    label: string;
+  };
+}
+
+interface Country {
+  name: {
+    common: string;
+  }
+  flag: string;
+  currencies: object;
+}
+
+const SelectCountry = (props: Props) => {
+  const {
+    currencyValue, label, setExchange,
+  } = props;
   const dispatch = useDispatch();
   const countriesData = useSelector((state: RootState) => state.countries.countriesData);
+  console.log(countriesData);
 
   const filteredCountries = countriesData.filter((country: object) => Object.prototype.hasOwnProperty.call(country, 'currencies'));
+
   const sortedCountries = filteredCountries
-    .sort((a: any, b: any) => a.name.common.localeCompare(b.name.common));
-  const formOptions = sortedCountries.map((country: any) => {
+    .sort((a: object, b: object) => {
+      const aCountry = a as Country;
+      const bCountry = b as Country;
+      return aCountry.name.common.localeCompare(bCountry.name.common);
+    });
+
+  const formOptions = sortedCountries.map((country: object) => {
+    const countryTypes = country as Country;
     const os = navigator.platform;
-    const [currency] = Object.keys(country.currencies);
-    const countryName = country.name.common;
-    const currencyData = `${os === 'Win32' ? '' : country.flag} ${currency} — ${countryName}`;
+    const [currency] = Object.keys(countryTypes.currencies);
+    const countryName = countryTypes.name.common;
+    const currencyData = `${os === 'Win32' ? '' : countryTypes.flag} ${currency} — ${countryName}`;
     return currencyData.trim();
   });
 
   return (
-    <Grid item xs={12} md={3}>
+    <Grid item xs={12} md={3.5}>
       <Autocomplete
         value={currencyValue}
         isOptionEqualToValue={(option, value) => option.trim() === value.trim()}
         disableClearable
-        onChange={(e, newValue) => dispatch(setCurrency(newValue))}
+        onChange={(e, newValue) => {
+          const action: SetExchangeAction = setExchange({ newValue, label });
+          dispatch(action);
+        }}
         options={formOptions}
         renderInput={(params) => <TextField {...params} label={label} />}
       />
