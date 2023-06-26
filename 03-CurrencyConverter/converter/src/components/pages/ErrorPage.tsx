f0ecaaa7dd9b7b8b3e9e13789a01d6cbe9b10af3
Max <ppmax@bk.ru>
2023-06-26 23:30:50 +0300

    Error page filled

--- 03-CurrencyConverter/converter/src/components/pages/ErrorPage.tsx
+++ 03-CurrencyConverter/converter/src/components/pages/ErrorPage.tsx
@@ -1,5 +1,13 @@
-const ErrorPage = () => (
-  <h1>Error</h1>
-);
+import { useNavigate } from 'react-router-dom';
+
+const ErrorPage = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="main-content">
+      <h1 className="converter-label">404: Страница не найдена</h1>
+      <button className="to-main-button" type="button" onClick={() => navigate('/')}>Вернуться на главную</button>
+    </div>
+  );
+};
 
 export default ErrorPage;
