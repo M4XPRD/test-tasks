56e2ed14e4f2f93ec6574af79190070658ada2ac
Max <ppmax@bk.ru>
2023-06-28 03:24:49 +0300

    Navigation fixes

--- 03-CurrencyConverter/converter/src/components/pages/ErrorPage.tsx
+++ 03-CurrencyConverter/converter/src/components/pages/ErrorPage.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from 'react-router-dom';
+import routes from '../../routes';
 
 const ErrorPage = () => {
   const navigate = useNavigate();
   return (
     <div className="main-content">
       <h1 className="converter-label">404: Страница не найдена</h1>
-      <button className="to-main-button" type="button" onClick={() => navigate('/')}>Вернуться на главную</button>
+      <button className="to-main-button" type="button" onClick={() => navigate(routes.main())}>Вернуться на главную</button>
     </div>
   );
 };
--- 03-CurrencyConverter/converter/src/components/pages/MainPage.tsx
+++ 03-CurrencyConverter/converter/src/components/pages/MainPage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { Container, Grid } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
 import { ThunkDispatch, AnyAction } from '@reduxjs/toolkit';
+import { useNavigate } from 'react-router-dom';
 import InputAmount from '../InputAmount';
 import SelectCountry from '../SelectCountry';
 import SwitchCurrency from '../SwitchCurrency';
@@ -10,9 +11,11 @@ import { AppDispatch, RootState } from '../../slices';
 import AnimationBar from '../AnimationBar';
 import { setExchange } from '../../slices/currenciesSlice';
 import ExchangeResult from '../ExchangeResult';
+import routes from '../../routes';
 
 const MainPage = () => {
   const dispatch = useDispatch<ThunkDispatch<RootState, AppDispatch, AnyAction>>();
+  const navigate = useNavigate();
   const loadingStatus = useSelector((state: RootState) => state.countries.loadingStatus);
   const exchangeFrom = useSelector((state: RootState) => state.currencies.exchangeFrom);
   const exchangeTo = useSelector((state: RootState) => state.currencies.exchangeTo);
@@ -36,6 +39,7 @@ const MainPage = () => {
             <ExchangeResult />
           </Container>
         )}
+      <button className="to-currencies-button" type="button" onClick={() => navigate(routes.currencies())}>Все курсы валют</button>
     </div>
   );
 };
