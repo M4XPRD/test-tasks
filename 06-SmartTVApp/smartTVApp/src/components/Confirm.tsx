4f9dad86a9f796a2ae390eea1ff31c96792bb67e
Max <ppmax@bk.ru>
2023-10-25 16:48:09 +0300

    Basic app navigation added

--- 06-SmartTVApp/smartTVApp/src/components/Confirm.tsx
+++ 06-SmartTVApp/smartTVApp/src/components/Confirm.tsx
@@ -1,34 +1,39 @@
 import QR110 from '../assets/QR-110.svg';
 import closeButtonDark from '../assets/close-button-dark.svg';
+import useApp from '../hooks/appHook';
 
-const Confirm = () => (
-  <div className="main-container">
-    <section className="left-block">
-      <div className="confirm-block">
-        <h1 className="confirm-h1">
-          <p>
-            ЗАЯВКА
-          </p>
-          <p>
-            ПРИНЯТА
-          </p>
-        </h1>
-        <div className="confirm-p">
-          <p>Держите телефон под рукой.</p>
-          <p>Скоро с Вами свяжется наш менеджер.</p>
+const Confirm = () => {
+  const { closeApp } = useApp();
+
+  return (
+    <div className="main-container">
+      <section className="left-block">
+        <div className="confirm-block">
+          <h1 className="confirm-h1">
+            <p>
+              ЗАЯВКА
+            </p>
+            <p>
+              ПРИНЯТА
+            </p>
+          </h1>
+          <div className="confirm-p">
+            <p>Держите телефон под рукой.</p>
+            <p>Скоро с Вами свяжется наш менеджер.</p>
+          </div>
         </div>
-      </div>
-    </section>
-    <section className="right-block">
-      <button type="button">
-        <img src={closeButtonDark} alt="Закрыть окно" />
-      </button>
-      <div className="qr-block">
-        <p>СКАНИРУЙТЕ QR-КОД ДЛЯ ПОЛУЧЕНИЯ ДОПОЛНИТЕЛЬНОЙ ИНФОРМАЦИИ</p>
-        <img src={QR110} alt="QR-код: дополнительная информация" />
-      </div>
-    </section>
-  </div>
-);
+      </section>
+      <section className="right-block">
+        <button type="button" onClick={() => closeApp()}>
+          <img src={closeButtonDark} alt="Закрыть окно" />
+        </button>
+        <div className="qr-block">
+          <p>СКАНИРУЙТЕ QR-КОД ДЛЯ ПОЛУЧЕНИЯ ДОПОЛНИТЕЛЬНОЙ ИНФОРМАЦИИ</p>
+          <img src={QR110} alt="QR-код: дополнительная информация" />
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Confirm;
